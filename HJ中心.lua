c3d16722bc77c9cd2b803ee17f9458223471ff56
nnzhonhxin <3762912107@qq.com>
2024-07-13 19:51:03 +0800

    Add files via upload

---
+++ HJ中心.lua
@@ -0,0 +1,279 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))() 
+ local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))() 
+ Notification:Notify( 
+     {Title = "SAY", Description = "纯逢合别骂我"}, 
+     {OutlineColor = Color3.fromRGB(155, 80, 80),Time = 3, Type = "image"}, 
+     {Image = "http://www.roblox.com/asset/?id=17240832325", ImageColor = Color3.fromRGB(255, 255, 255)} 
+ )
+ wait(1)
+ local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))() 
+ local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))() 
+ Notification:Notify( 
+     {Title = "作者", Description = "by:ghgj"}, 
+     {OutlineColor = Color3.fromRGB(155, 80, 80),Time = 3, Type = "image"}, 
+     {Image = "http://www.roblox.com/asset/?id=17240832325", ImageColor = Color3.fromRGB(255, 255, 255)} 
+ )
+ wait(2 )
+
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/VeaMSRZK"))()
+local LBLG = Instance.new("ScreenGui", getParent)
+local LBL = Instance.new("TextLabel", getParent)
+local player = game.Players.LocalPlayer
+
+LBLG.Name = "LBLG"
+LBLG.Parent = game.CoreGui
+LBLG.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+LBLG.Enabled = true
+LBL.Name = "LBL"
+LBL.Parent = LBLG
+LBL.BackgroundColor3 = Color3.new(1, 1, 1)
+LBL.BackgroundTransparency = 1
+LBL.BorderColor3 = Color3.new(0, 0, 0)
+LBL.Position = UDim2.new(0.75,0,0.010,0)
+LBL.Size = UDim2.new(0, 133, 0, 30)
+LBL.Font = Enum.Font.GothamSemibold
+LBL.Text = "纯缝合"
+LBL.TextColor3 = Color3.new(155, 155, 155)
+LBL.TextScaled = true
+LBL.TextSize = 14
+LBL.TextWrapped = true
+LBL.Visible = true
+
+local FpsLabel = LBL
+local Heartbeat = game:GetService("RunService").Heartbeat
+local LastIteration, Start
+local FrameUpdateTable = { }
+
+local function HeartbeatUpdate()
+	LastIteration = tick()
+	for Index = #FrameUpdateTable, 1, -1 do
+		FrameUpdateTable[Index + 1] = (FrameUpdateTable[Index] >= LastIteration - 1) and FrameUpdateTable[Index] or nil
+	end
+	FrameUpdateTable[1] = LastIteration
+	local CurrentFPS = (tick() - Start >= 1 and #FrameUpdateTable) or (#FrameUpdateTable / (tick() - Start))
+	CurrentFPS = CurrentFPS - CurrentFPS % 1
+	FpsLabel.Text = ("北京时间:"..os.date("%H").."时"..os.date("%M").."分"..os.date("%S")).."秒"
+end
+
+OrionLib:MakeNotification({
+                    Name = "正在开启反挂机请稍等...",
+                    Content = "Akf 开启中...",
+                    Time = 4
+                })     
+print("索引开启反挂机")
+		local vu = game:GetService("VirtualUser")
+		game:GetService("Players").LocalPlayer.Idled:connect(function()
+		   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		   wait(1)
+		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		end)      
+                wait(4)
+OrionLib:MakeNotification({
+                    Name = "开启成功",
+                    Content = "AKF",
+                    Time = 3
+                })
+                OrionLib:MakeNotification({
+                    Name = "开心",
+                    Content = "开心对待每一天",
+                    Time = 3
+                })
+    wait(1)
+                
+Start = tick()
+Heartbeat:Connect(HeartbeatUpdate)
+local Window = OrionLib:MakeWindow({Name = "HJ中心1.0", HidePremium = false, SaveConfig =true,IntroText="HJ中心1.0", ConfigFolder = "NN中心"})
+local Tab = Window:MakeTab({
+	Name = "主页",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddParagraph("更新","通用,doors,自然灾害,忍者传奇(忍者传奇之更新到雷暴岛)")
+Tab:AddParagraph("云端更新","wh")
+Tab:AddParagraph("永久免费")
+Tab:AddParagraph("提示","脚本中心") 
+Tab:AddParagraph("你的注入器:"..identifyexecutor())
+Tab:AddParagraph("作者","作者不愿意透露自己")
+Tab:AddParagraph("别骂")
+
+Tab:AddButton ({
+	Name = "复制",
+	Callback = function ()
+	 setclipboard("没有东西")
+	 OrionLib:MakeNotification({
+                    Name = "提示信息",
+                    Content = "你已复制，请在粘贴板中查看",
+                    Time = 3
+                })
+	end
+})
+
+local Tab = Window:MakeTab({
+	Name = "通用",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddTextbox({
+
+	Name = "移动速度设置",
+
+	Default = "",
+
+	TextDisappear = true,
+
+	Callback = function(Value)
+
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+		
+		
+	end
+})
+Tab:AddTextbox({
+	Name = "重力设置",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+		game.Workspace.Gravity = Value
+		
+	end
+})
+Tab:AddTextbox({
+	Name = "跳跃高度设置",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+			end
+})
+local Section = Tab:AddSection({
+	Name = "飞行"
+})
+Tab:AddButton ({
+	Name = "飞行v3",
+	Callback = function ()
+	 loadstring(game:HttpGet("https://pastebin.com/raw/UVAj0uWu"))()
+	end
+})
+Tab:AddButton ({
+	Name = "阿尔宙斯飞行",
+	Callback = function ()
+	 loadstring(game:HttpGet("https://pastebin.com/raw/5zJu3hfN"))()
+	end
+})
+local Tab = Window:MakeTab({
+	Name = "doors",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddButton ({
+	Name = "ms",
+	Callback = function ()
+	 loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSHUB_Loader.lua"),true))()
+	end
+})
+Tab:AddButton ({
+	Name = "键盘脚本",
+	Callback = function ()
+	 loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+	end
+})
+Tab:AddButton ({
+	Name = "变成怪物",
+	Callback = function ()
+	 loadstring(game:HttpGet("https://raw.githubusercontent.com/ChronoAccelerator/Public-Scripts/main/Morphing/MorphScript.lua"))();
+	end
+})
+Tab:AddButton ({
+	Name = "floor2(改地图)",
+	Callback = function ()
+	 loadstring(game:HttpGet("https://raw.githubusercontent.com/iCherryKardes/Doors/main/Floor%202%20Mod"))()
+	end
+})
+local Section = Tab:AddSection({
+	Name = "飞行"
+})
+Tab:AddButton ({
+	Name = "seek枪",
+	Callback = function ()
+	 loadstring(game:HttpGet("https://raw.githubusercontent.com/persopoiu/scripts/main/seekgun.lua"))()
+	end
+})
+Tab:AddButton ({
+	Name = "臭猫",
+	Callback = function ()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/K0t1n/Public/main/Maxwell Plushie"))()
+	end
+})
+Tab:AddButton ({
+	Name = "圣光手雷",
+	Callback = function ()
+	 loadstring(game:HttpGet("https://raw.githubusercontent.com/MrNeRD0/Doors-Hack/main/HolyGrenadeByNerd.lua"))()
+	end
+})
+Tab:AddButton ({
+	Name = "磁铁",
+	Callback = function ()
+	 loadstring(game:HttpGet("https://raw.githubusercontent.com/MrNeRD0/Doors-Hack/main/MagnetByNerd.lua"))()
+	end
+})
+local Tab = Window:MakeTab({
+	Name = "自然灾害模拟器",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton ({
+	Name = "回到出生点",
+	Callback = function ()
+	 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-252.8938751220703, 180.0355987548828, 321.2361145019531)
+	end
+})
+local Tab = Window:MakeTab({
+	Name = "忍者传奇",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton ({
+	Name = "附魔岛",
+	Callback = function ()
+	 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(83.028564453125, 766.3915405273438, -128.62686157226562)
+	end
+})
+Tab:AddButton ({
+	Name = "星界岛",
+	Callback = function ()
+	 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(144.43006896972656, 2014.091064453125, 247.5571746826172)
+	end
+})
+Tab:AddButton ({
+	Name = "神秘岛",
+	Callback = function ()
+	 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(162.7420654296875, 4047.7841796875, 13.378257751464844)
+	end
+})
+Tab:AddButton ({
+	Name = "太空岛",
+	Callback = function ()
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(184.0364227294922, 5657.091796875, 161.54000854492188)
+	end
+})
+Tab:AddButton ({
+	Name = "冻土岛",
+	Callback = function ()
+	 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(186.7508544921875, 9285.08984375, 158.16287231445312)
+	end
+})
+Tab:AddButton ({
+	Name = "沙暴",
+	Callback = function ()
+	 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(183.7511749267578, 17686.236328125, 147.5008087158203)
+	end
+})
+Tab:AddButton ({
+	Name = "雷暴",
+	Callback = function ()
+	 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(186.87640380859375, 24069.9296875, 158.25582885742188)
+	end
+})
